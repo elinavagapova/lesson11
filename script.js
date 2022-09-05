03138deaed740a8d9e1183593b4a844eaf152627
Elina Vagapova <vagapovaelina98@gmail.com>
2022-09-05 19:30:18 +0300

    lesson11 выполнено

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const text = document.querySelector('#text');
+const btn = document.querySelector('#btn');
+const square = document.querySelector('#square');
+const btn1 = document.querySelector('#e_btn');
+const range = document.querySelector('#range');
+const circle = document.querySelector('#circle');
+
+
+const color = function(event) {
+
+  const color1 = function() {
+    square.style.backgroundColor = event.target.value;
+  };
+
+  btn.addEventListener('click', color1);
+};
+
+text.addEventListener('input', color);
+
+btn1.style.display = 'none';
+
+const diapason = function(event) {
+  circle.style.width = event.target.value + "%";
+  circle.style.height = event.target.value + "%";
+};
+
+range.addEventListener('input', diapason);
 
 
 
